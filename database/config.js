41be4367f1745a2557e2ac05c780ed10f0b40acd
Rafly Herdianto <raflyherdianto25@gmail.com>
2025-06-10 05:55:18 +0700

    refactor: simplify database configuration by using environment variables

--- database/config.js
+++ database/config.js
@@ -1,35 +1,36 @@
 'use strict';
 
-const appConfig = require('../config');
+require('dotenv').config(); 
 
 module.exports = {
   development: {
-    username: appConfig.environments.development.username,
-    password: appConfig.environments.development.password,
-    database: appConfig.environments.development.database,
-    host: appConfig.environments.development.host,
-    port: appConfig.environments.development.port,
-    dialect: appConfig.environments.development.dialect,
-    logging: appConfig.environments.development.logging,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'backend_capstone',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: parseInt(process.env.DB_PORT, 10) || 5433,
+    dialect: 'postgres'
   },
   test: {
-    username: appConfig.environments.test.username,
-    password: appConfig.environments.test.password,
-    database: appConfig.environments.test.database,
-    host: appConfig.environments.test.host,
-    port: appConfig.environments.test.port,
-    dialect: appConfig.environments.test.dialect,
-    logging: appConfig.environments.test.logging,
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: parseInt(process.env.DB_PORT_TEST, 10),
+    dialect: 'postgres'
   },
   production: {
-    username: appConfig.environments.production.username,
-    password: appConfig.environments.production.password,
-    database: appConfig.environments.production.database,
-    host: appConfig.environments.production.host,
-    port: appConfig.environments.production.port,
-    dialect: appConfig.environments.production.dialect,
-    logging: appConfig.environments.production.logging,
-    // Jika Anda menggunakan dialectOptions untuk production (misalnya SSL)
-    // dialectOptions: appConfig.environments.production.dialectOptions || {},
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    port: parseInt(process.env.DB_PORT_PROD, 10) || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
